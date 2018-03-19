97ade48ecd6afefd808716bd6c63c47a1015ac03
vamour_y <yohan.vamour@epitech.eu>
2018-03-19 17:14:54 +0100

    Image dans la map

--- app/src/main/java/neonews/neonews/MapsActivity.java
+++ app/src/main/java/neonews/neonews/MapsActivity.java
@@ -1,6 +1,12 @@
 package neonews.neonews;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
@@ -16,7 +22,14 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -41,18 +54,49 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
-        Intent i = getIntent();
-        ArrayList<Subject> list = (ArrayList<Subject>) i.getSerializableExtra("ListSubject");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = getIntent();
+                ArrayList<Subject> list = (ArrayList<Subject>) i.getSerializableExtra("ListSubject");
 
-        for (Subject sub : list) {
-            LatLng pos = new LatLng(sub.getX(), sub.getY());
-            BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.newspaper);
-            mMap.addMarker(new MarkerOptions().position(pos).title(sub.getTitle()).icon(icon));
-        }
+                Paint color = new Paint();
+                color.setTextSize(35);
+                color.setColor(Color.WHITE);
+
+                for (final Subject sub : list) {
+                    final LatLng pos = new LatLng(sub.getX(), sub.getY());
+                    Bitmap myBitmap = null;
+                    try {
+                        URL url = new URL(sub.getImageUrl());
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setDoInput(true);
+                        connection.connect();
+                        InputStream input = connection.getInputStream();
+                        myBitmap = BitmapFactory.decodeStream(input);
+                    } catch (IOException e) {
+                        // Log exception
+                    }
+                    Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+                    Bitmap bmp = Bitmap.createBitmap(80, 80, conf);
+                    Canvas canvas1 = new Canvas(bmp);
+                    canvas1.drawBitmap(myBitmap, 0,0, color);
+                    canvas1.drawText(sub.title , 30, 40, color);
+                    final Bitmap finalMyBitmap = myBitmap;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mMap.addMarker(new MarkerOptions().position(pos).title(sub.getTitle()).icon(BitmapDescriptorFactory.fromBitmap(finalMyBitmap)).anchor(0.5f, 1));
+                        }
+                    });
+                }
+            }
+        }).start();
+
         LatLng lille = new LatLng(50.633333, 3.066667);
-        mMap.addMarker(new MarkerOptions().position(lille).title("Marker in Lille"));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lille, 12.0f));
     }
 
