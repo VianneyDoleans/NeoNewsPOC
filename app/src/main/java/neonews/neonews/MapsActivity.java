f40db28477f9cdedad96d990b77bedc173dbebf4
vamour_y <yohan.vamour@epitech.eu>
2018-03-20 15:43:57 +0100

    Can click on maps news

--- app/src/main/java/neonews/neonews/MapsActivity.java
+++ app/src/main/java/neonews/neonews/MapsActivity.java
@@ -1,5 +1,6 @@
 package neonews.neonews;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -21,6 +22,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
@@ -35,6 +37,8 @@ import java.util.ArrayList;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    ArrayList<Subject> _list;
+    Context _Ctx = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,13 +64,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void run() {
                 Intent i = getIntent();
-                ArrayList<Subject> list = (ArrayList<Subject>) i.getSerializableExtra("ListSubject");
+                _list = (ArrayList<Subject>) i.getSerializableExtra("ListSubject");
 
                 Paint color = new Paint();
                 color.setTextSize(35);
                 color.setColor(Color.WHITE);
 
-                for (final Subject sub : list) {
+                for (final Subject sub : _list) {
                     final LatLng pos = new LatLng(sub.getX(), sub.getY());
                     Bitmap myBitmap = null;
                     try {
@@ -79,33 +83,37 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     } catch (IOException e) {
                         // Log exception
                     }
-                    Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-                    Bitmap bmp = Bitmap.createBitmap(80, 80, conf);
-                    Canvas canvas1 = new Canvas(bmp);
-                    canvas1.drawBitmap(myBitmap, 0,0, color);
-                    canvas1.drawText(sub.title , 30, 40, color);
                     final Bitmap finalMyBitmap = myBitmap;
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            mMap.addMarker(new MarkerOptions().position(pos).title(sub.getTitle()).icon(BitmapDescriptorFactory.fromBitmap(finalMyBitmap)).anchor(0.5f, 1));
+                            mMap.addMarker(new MarkerOptions().position(pos)
+                                    .title(sub.getTitle())
+                                    .icon(BitmapDescriptorFactory.fromBitmap(finalMyBitmap))
+                                    .anchor(0.5f, 1)).showInfoWindow();
                         }
                     });
                 }
             }
         }).start();
 
-        LatLng lille = new LatLng(50.633333, 3.066667);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lille, 12.0f));
-    }
+        LatLng center = new LatLng(46.679603, 1.636908);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(center, 5.5f));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
-        }
-        return true;
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                int position = 0;
+                for (Subject sub : _list) {
+                    if (sub.getTitle().equals(marker.getTitle()))
+                        break;
+                    position++;
+                }
+                Subject subject = _list.get(position);
+                AlertChooseMedia alert = new AlertChooseMedia(_Ctx, subject);
+                alert.show();
+                return true;
+            }
+        });
     }
 }
